a4eae653b3bf84d4fc3c4558fd0d7cb41bedd0b0
Mikkeep <mikke.pennanen@gmail.com>
2022-04-09 23:04:32 +0300

    Added settings page as new window

--- cryptor.py
+++ cryptor.py
@@ -4,10 +4,12 @@
     decryption of files."""
 
 
+from cProfile import label
 import sys
 import json
 from PyQt5.QtWidgets import *
-from PyQt5 import QtGui, QtCore
+from PyQt5 import QtGui, QtCore, Qt, QtWidgets
+from qtwidgets import PasswordEdit
 from constants import *
 from utils import *
 
@@ -16,10 +18,87 @@ __author__ = "Mikael Pennanen & Juho Bruun"
 __version__ = "1.0"
 __name__ = "Cryptor"
 
+class SettingsWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.chosen_algo = ""
+        self.chosen_salt = ""
+        self.chosen_key = ""
+        self.settings_translate = Window().translations
+        enc_key = self.settings_translate["buttons"]["encryption_key_prompt"]
+        enc_key_confirm = self.settings_translate["buttons"]["encryption_key_confirm"]
+        algorithm = self.settings_translate["buttons"]["algorithm"]
+        salt = self.settings_translate["buttons"]["salt"]
+        close_btn = self.settings_translate["prompts"]["close_button"]
+
+        self.algorithm = QPushButton(algorithm)
+        self.text_box_salt = PasswordEdit(self)
+        self.text_box_salt.setPlaceholderText(salt)
+        self.text_box_enc_text = PasswordEdit(self)
+        self.text_box_enc_text.setPlaceholderText(enc_key)
+        self.text_box_enc_text_confirm = PasswordEdit(self)
+        self.text_box_enc_text_confirm.setPlaceholderText(enc_key_confirm)
+        self.close_button = QPushButton(close_btn)
+        self.close_button.clicked.connect(self.close_settings)
+        self.menu = QMenu(self)
+        self.menu.addAction('MD5')
+        self.menu.addSeparator()
+        self.menu.addAction('SHA-256')
+        self.menu.addSeparator()
+        self.menu.addAction('SHA-512')
+        self.menu.addSeparator()
+        self.menu.addAction('SHA3-512')
+        self.algorithm.setMenu(self.menu)
+        self.menu.triggered.connect(self.algorithms)
+        layout.addWidget(self.algorithm)
+#        layout.addWidget(self.salt)
+        layout.addWidget(self.text_box_salt)
+        layout.addWidget(self.text_box_enc_text)
+        layout.addWidget(self.text_box_enc_text_confirm)
+        layout.addSpacing(50)
+        layout.addWidget(self.close_button)
+        self.setLayout(layout)
+
+        self.Width = 570
+        self.height = int(0.6 * self.Width)
+        self.setFixedSize(self.Width, self.height)
+        # center the window relative to screensize
+        centering = self.frameGeometry()
+        centerOfScreen = QDesktopWidget().availableGeometry().center()
+        centering.moveCenter(centerOfScreen)
+        self.move(centering.topLeft())
+
+
+    def algorithms(self, language):
+        self.chosen_algo = language.text()
+        print(language.text())
+        # msg = QMessageBox()
+        # display = msg.exec_()
+        return language
+
+    def close_settings(self, event):
+        pwd_mismatch = self.settings_translate["prompts"]["password_mismatch"]
+        print("Chosen algorithm: ", self.chosen_algo)
+        print("Chosen salt: ", self.text_box_salt.text())
+        print("Chosen enc key: ", self.text_box_enc_text.text())
+        print("Chosen enc key: ", self.text_box_enc_text_confirm.text())
+        if self.text_box_enc_text != self.text_box_enc_text_confirm:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Warning)
+            msg.setText(pwd_mismatch)
+            display = msg.exec_()
+            return
+
+        self.close()
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.w = None
+
+        self.mdi = QMdiArea()
 
         CURRENT_LANG = read_used_lang(db_location)
         self.translations = self.read_translation(CURRENT_LANG)
@@ -141,6 +220,29 @@ class Window(QMainWindow):
         display = msg.exec_()
         return language
 
+    def button_language(self, language):
+        start_up_lang_info = self.translations["prompts"]["language_selection_info"]
+        print(language.text())
+        write_used_lang(db_location, (language.text(),))
+        msg = QMessageBox()
+        msg.setText(start_up_lang_info)
+        display = msg.exec_()
+        return language
+
+    def default_encrypt_window(self):
+        
+        if self.w is None:
+            self.w = SettingsWindow()
+        self.w.show()
+        self.sub = QMdiSubWindow()
+#        self.sub.setWidget(QTextEdit("MOI"))
+        self.sub.setWindowTitle("subwindow")
+        self.mdi.addSubWindow(self.sub)
+        self.sub.show()
+
+    def close_subwindow(self):
+        self.mdi.close()
+
     def encryption(self):
         final_layout = QVBoxLayout()
 
@@ -217,31 +319,59 @@ class Window(QMainWindow):
 
     def settings(self):
         start_up_lang = self.translations["prompts"]["startup_lang"]
+        start_up_encrypt = self.translations["prompts"]["default_encrypt"]
         self.main_layout = QVBoxLayout()
-        self.main_layout.addWidget(QLabel(start_up_lang))
-        self.lang_list = QComboBox()
-        self.lang_list.addItem("English")
-        self.lang_list.addItem("Suomi")
-        self.lang_list.addItem("Svenska")
-        self.lang_list.currentIndexChanged.connect(self.button_choose_lang)
-        self.main_layout.addWidget(self.lang_list)
-        self.main_layout.addStretch(5)
+#        self.main_layout.addWidget(QLabel(start_up_lang))
+        self.button_lang = QPushButton(start_up_lang, self)
+        menu = QMenu(self)
+        menu.addAction('English')
+        menu.addSeparator()
+        menu.addAction('Suomi')
+        menu.addSeparator()
+        menu.addAction('Svenska')
+        self.button_lang.setMenu(menu)
+        menu.triggered.connect(self.button_language)
+        self.main_layout.addWidget(self.button_lang)
+#        self.lang_list = QComboBox()
+#        self.lang_list.addItem("English")
+#        self.lang_list.addItem("Suomi")
+#        self.lang_list.addItem("Svenska")
+#        self.lang_list.currentIndexChanged.connect(self.button_choose_lang)
+#        self.main_layout.addWidget(self.lang_list)
+#        self.main_layout.addStretch(5)
+#        self.main_layout.addWidget(QLabel(start_up_encrypt))
+        self.button_default_crypt = QPushButton(start_up_encrypt, self)
+        self.main_layout.addWidget(self.button_default_crypt)
+        self.button_default_crypt.clicked.connect(self.default_encrypt_window)
         main = QWidget()
         main.setLayout(self.main_layout)
         return main
 
     def tab_enc_text(self):
-        bottom_layout = QVBoxLayout()
-        bottom_layout.addWidget(QLabel(":)"))
-        bottom_layout.addStretch(5)
+        self.bottom_layout = QVBoxLayout()
+        self.leiska2 = QVBoxLayout()
+        self.bottom_layout.setContentsMargins(0,0,0,0)
+        self.bottom_layout.setSpacing(0)
+#        bottom_layout.addStretch(5)
+        widget = QLabel("Hello")
+        font = widget.font()
+        font.setPointSize(10)
+        widget.setFont(font)
+        self.text_box_enc_text = PasswordEdit(self)
+        self.text_box_enc_text_confirm = PasswordEdit(self)
+        label_1 = QLabel()
+        label_1.setText("Encryption here: ")
+        self.bottom_layout.addWidget(widget)
+        self.bottom_layout.addWidget(self.text_box_enc_text)
+        LOL = self.bottom_layout.addWidget(self.text_box_enc_text_confirm)
         main = QWidget()
-        main.setLayout(bottom_layout)
+        main.setLayout(self.bottom_layout)
         return main
 
     def tab_enc_files(self):
         bottom_actions = QVBoxLayout()
         bottom_actions.addWidget(QLabel("(:"))
-        bottom_actions.addStretch(5)
+#        bottom_actions.addStretch(5)
         main = QWidget()
         main.setLayout(bottom_actions)
         return main
@@ -249,7 +379,7 @@ class Window(QMainWindow):
     def tab_dec_text(self):
         bottom_layout = QVBoxLayout()
         bottom_layout.addWidget(QLabel(":)"))
-        bottom_layout.addStretch(5)
+#        bottom_layout.addStretch(5)
         main = QWidget()
         main.setLayout(bottom_layout)
         return main
