7e61ce9427a0ab11beb36625e6de4017d8b51c9e
jburn <55876927+jburn@users.noreply.github.com>
2022-04-11 16:36:29 +0300

    decryption page fix

--- cryptor/pages/decryption.py
+++ cryptor/pages/decryption.py
@@ -1,38 +1,63 @@
 from PyQt5.QtWidgets import *
 
-def decryption(self):
+class Decrypt_page():
+	def __init__(self, translations):
+		self.translations = translations
+	
+	def button_dec_t(self):
+		self.bottom_widget_dec.setCurrentIndex(0)
 
-        final_layout = QVBoxLayout()
+	def button_dec_f(self):
+		self.bottom_widget_dec.setCurrentIndex(1)
 
-        dec_button_text = self.translations["buttons"]["decrypt_text"]
-        dec_button_files = self.translations["buttons"]["decrypt_files"]
+	def decryption(self):
+		final_layout = QVBoxLayout()
 
-        self.btn_dec_t = QPushButton(f"{dec_button_text}", self)
-        self.btn_dec_t.clicked.connect(self.button_dec_t)
-        self.btn_dec_f = QPushButton(f"{dec_button_files}", self)
-        self.btn_dec_f.clicked.connect(self.button_dec_f)
+		dec_button_text = self.translations["buttons"]["decrypt_text"]
+		dec_button_files = self.translations["buttons"]["decrypt_files"]
 
-        self.tab_dec_t = self.tab_dec_text()
-        self.tab_dec_f = self.tab_dec_files()
+		self.btn_dec_t = QPushButton(f"{dec_button_text}")
+		self.btn_dec_t.clicked.connect(self.button_dec_t)
+		self.btn_dec_f = QPushButton(f"{dec_button_files}")
+		self.btn_dec_f.clicked.connect(self.button_dec_f)
 
-        top_actions = QHBoxLayout()
-        top_actions.addWidget(self.btn_dec_t)
-        top_actions.addWidget(self.btn_dec_f)
-        self.top_widget_dec = QWidget()
-        self.top_widget_dec.setLayout(top_actions)
+		self.tab_dec_t = self.tab_dec_text()
+		self.tab_dec_f = self.tab_dec_files()
 
-        self.bottom_widget_dec = QTabWidget()
-        self.bottom_widget_dec.tabBar().setObjectName("DecryptionTab")
+		top_actions = QHBoxLayout()
+		top_actions.addWidget(self.btn_dec_t)
+		top_actions.addWidget(self.btn_dec_f)
+		self.top_widget_dec = QWidget()
+		self.top_widget_dec.setLayout(top_actions)
 
-        self.bottom_widget_dec.addTab(self.tab_dec_t, "")
-        self.bottom_widget_dec.addTab(self.tab_dec_f, "")
+		self.bottom_widget_dec = QTabWidget()
+		self.bottom_widget_dec.tabBar().setObjectName("DecryptionTab")
 
-        self.bottom_widget_dec.setCurrentIndex(0)
+		self.bottom_widget_dec.addTab(self.tab_dec_t, "")
+		self.bottom_widget_dec.addTab(self.tab_dec_f, "")
 
-        final_layout.addWidget(self.top_widget_dec)
-        final_layout.addWidget(self.bottom_widget_dec)
+		self.bottom_widget_dec.setCurrentIndex(0)
 
-        main = QWidget()
-        main.setLayout(final_layout)
+		final_layout.addWidget(self.top_widget_dec)
+		final_layout.addWidget(self.bottom_widget_dec)
 
-        return main
+		main = QWidget()
+		main.setLayout(final_layout)
+
+		return main
+
+	def tab_dec_text(self):
+		bottom_layout = QVBoxLayout()
+		bottom_layout.addWidget(QLabel(":)"))
+		#        bottom_layout.addStretch(5)
+		main = QWidget()
+		main.setLayout(bottom_layout)
+		return main
+
+	def tab_dec_files(self):
+		bottom_actions = QVBoxLayout()
+		bottom_actions.addWidget(QLabel("(:"))
+		bottom_actions.addStretch(5)
+		main = QWidget()
+		main.setLayout(bottom_actions)
+		return main
