590389f10228f9eef30f2d9c1b5d960322a2fbce
Mikkeep <mikke.pennanen@gmail.com>
2022-04-12 12:04:49 +0300

    Changed SQL queries to ones imported from constants and made check for darkmode

--- cryptor/utils.py
+++ cryptor/utils.py
@@ -1,3 +1,4 @@
+from multiprocessing import connection
 import sqlite3
 from constants import *
 from sqlite3 import Error, IntegrityError
@@ -8,27 +9,37 @@ def read_used_lang(db_name):
     try:
         conn = sqlite3.connect(db_name)
         cur = conn.cursor()
-        cur.execute("""SELECT LANGUAGE FROM LANG""")
+        cur.execute(read_lang)
         data = cur.fetchone()
         data = data[0]
         conn.close()
     except Error as e:
-        used_lang = "English"
+        data = "English"
         print("Could not read used language from db")
     return data
 
-
-def create_db_table(db_name):
+def insert_to_db(db_name, sql_command):
     connection = sqlite3.connect(db_name)
     cur = connection.cursor()
     try:
-        cur.execute(table_lang)
+        cur.execute(sql_command)
+        connection.commit()
+        connection.close()
+    except IntegrityError as e:
+        print("Cant insert data")
+        return
+    print("Data inserted succesfully")
+    
+
+def create_db_table(db_name, sql_command):
+    connection = sqlite3.connect(db_name)
+    cur = connection.cursor()
+    try:
+        cur.execute(sql_command)
+        connection.commit()
+        connection.close()
     except IntegrityError as e:
         print("table already exists")
-    cur.execute(insert_to_lang)
-    connection.commit()
-    connection.close()
-    print("Database created succesfully")
 
 
 def check_db(db_name):
@@ -44,12 +55,51 @@ def check_db(db_name):
         if connection:
             cur = connection.cursor()
             try:
-                cur.execute("SELECT * from LANG;")
+                cur.execute(read_from_lang)
+                connection.close()
             except sqlite3.OperationalError as e:
                 print("Database does not exist, creating a new one")
-                create_db_table(db_name)
+                create_db_table(db_name, table_lang)
+                create_db_table(db_name, table_mode)
+                insert_to_db(db_name, insert_to_lang)
+                insert_to_db(db_name, insert_to_mode)
                 connection.close()
 
+def check_dark_mode(db_name):
+    """
+    Check whetever user has selected to use dark mode
+    """
+    data = ""
+    try:
+        conn = sqlite3.connect(db_name)
+        cur = conn.cursor()
+        cur.execute(read_mode)
+        data = cur.fetchone()
+        data = data[0]
+        conn.close()
+    except Error as e:
+        data = "False"
+        print("Could not read used mode from db")
+    return data
+
+def write_used_mode(db_name, mode):
+    # False is used for Non-darkmode
+    # True is for darkmode
+    connection = None
+    cur = None
+    try:
+        connection = sqlite3.connect(db_name)
+        cur = connection.cursor()
+    except Error as e:
+        print(e)
+    finally:
+        sql_command = """ UPDATE MODE
+                            SET Darkmode = ?
+        """
+        cur.execute(sql_command, mode)
+        print(f"Changed darkmode to {mode}")
+        connection.commit()
+        connection.close()
 
 def write_used_lang(db_name, language):
     connection = None
