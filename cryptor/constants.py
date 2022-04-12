5958879965a2abcdb4b54fdfcd3c31079a0f8e37
Mikkeep <mikke.pennanen@gmail.com>
2022-04-12 12:00:51 +0300

    Changed all SQL queries to constant from utils for better management

--- cryptor/constants.py
+++ cryptor/constants.py
@@ -13,4 +13,18 @@ insert_to_lang = """ INSERT INTO LANG VALUES (
                     'English'
         ); """
 
-read_from_lang = """SELECT * FROM LANG"""
+read_from_lang = """SELECT * FROM LANG;"""
+
+read_lang = """SELECT LANGUAGE FROM LANG;"""
+
+table_mode = """ CREATE TABLE MODE (
+                Darkmode BOOLEAN NOT NULL
+        ); """
+
+insert_to_mode = """ INSERT INTO MODE VALUES (
+                        'False'
+        ); """
+
+read_from_mode = """SELECT * FROM MODE;"""
+
+read_mode = """SELECT DARKMODE FROM MODE;"""
