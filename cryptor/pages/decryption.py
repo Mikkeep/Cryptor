6edcd7b103a65328ab3e839d8a30415e5e92e393
Juho B <jopbruun@gmail.com>
2022-04-14 09:29:29 +0300

    first version of gui finished

--- cryptor/pages/decryption.py
+++ cryptor/pages/decryption.py
@@ -15,8 +15,12 @@ class Decrypt_page:
         self.bottom_widget_dec.setCurrentIndex(1)
 
     def decryption(self):
+        """
+        This method handles the frame for the entire decrypt tab
+        """
         final_layout = QVBoxLayout()
 
+        # DEFINE TOP WIDGET (TABS AND SWITCHING BETWEEN THEM)
         dec_button_text = self.translations["buttons"]["decrypt_text"]
         dec_button_files = self.translations["buttons"]["decrypt_files"]
 
@@ -25,32 +29,38 @@ class Decrypt_page:
         self.btn_dec_f = QPushButton(f"{dec_button_files}")
         self.btn_dec_f.clicked.connect(self.button_dec_f)
 
-        self.tab_dec_t = self.tab_dec_text()
-        self.tab_dec_f = self.tab_dec_files()
-
         top_actions = QHBoxLayout()
         top_actions.addWidget(self.btn_dec_t)
         top_actions.addWidget(self.btn_dec_f)
         self.top_widget_dec = QWidget()
         self.top_widget_dec.setLayout(top_actions)
 
+        # DEFINE BOTTOM WIDGET (TAB CONTENTS)
+        self.tab_dec_t = self.tab_dec_text()
+        self.tab_dec_f = self.tab_dec_files()
         self.bottom_widget_dec = QTabWidget()
         self.bottom_widget_dec.tabBar().setObjectName("DecryptionTab")
 
         self.bottom_widget_dec.addTab(self.tab_dec_t, "")
         self.bottom_widget_dec.addTab(self.tab_dec_f, "")
 
-        self.bottom_widget_dec.setCurrentIndex(0)
+        self.bottom_widget_dec.setCurrentIndex(0) # default to text decryption tab
 
+        # add top and bottom widgets to layout
         final_layout.addWidget(self.top_widget_dec)
         final_layout.addWidget(self.bottom_widget_dec)
 
+        # Finish layout
         main = QWidget()
         main.setLayout(final_layout)
 
         return main
 
     def tab_dec_text(self):
+        """
+        This method handles the text decryption tab
+        """
+        # init layout and set suitable column widths
         layout = QGridLayout()
         layout.setColumnStretch(0, 1)
         layout.setColumnStretch(1, 2)
@@ -98,6 +108,10 @@ class Decrypt_page:
         self._save = FileDialog().fileSave()
 
     def tab_dec_files(self):
+        """
+        This method handles the file decryption tab
+        """
+        # init layout and set suitable column widths
         layout = QGridLayout()
         layout.setColumnStretch(0, 1)
         layout.setColumnStretch(1, 2)
@@ -135,6 +149,7 @@ class Decrypt_page:
         decrypt_button = QPushButton(self.translations["buttons"]["final_decrypt"])
         layout.addWidget(decrypt_button, 3, 0, 1, 4)
         
+        # finish layout
         main = QWidget()
         main.setLayout(layout)
         return main
