b936d69043e0275b9a606efbdb80708ff9152d9c
Mikkeep <mikke.pennanen@gmail.com>
2022-04-14 09:18:59 +0300

    Added functionality to encrypt with AES to encrypt files tab. Hardcoded at the moment to test

--- cryptor/pages/encryption.py
+++ cryptor/pages/encryption.py
@@ -1,12 +1,18 @@
+import os
 from PyQt5.QtWidgets import *
 from qtwidgets import PasswordEdit
 from .file_dialog import FileDialog
 from constants import ENC_ALGORITHMS
+from crypto import encrypt
 
 
 class Encrypt_page:
     def __init__(self, translations):
+        # Define used class parameters to be set in the selections
         self.translations = translations
+        self.filepath = ""
+        self.salt = ""
+        self.enc_key = ""
 
     def button_enc_t(self):
         self.bottom_widget.setCurrentIndex(0)
@@ -122,12 +128,34 @@ class Encrypt_page:
         File dialog opening method
         """
         self._files = FileDialog().fileOpen()
+        self.filepath = self._files
 
     def filedialogsave(self):
         """
         File save method
         """
         self._save = FileDialog().fileSave()
+
+    #Encrypt parameters set and function call
+    def encrypt_file(self, filepath="", salt="", enc_key=""):
+        self.enc_key = self.text_box_enc_text_confirm.text()
+        self.salt = self.salt_insert_box.text()
+        filepath = self.filepath
+        # File out gets the name of the file for saving the file
+        fileout = os.path.basename(self.filepath)
+        print(fileout)
+        salt = self.salt
+        enc_key = self.enc_key
+        print("clicked encrypt")
+        print("Filepath: ", self.filepath)
+        print("used password: ", self.enc_key)
+        print("used salt: ", self.salt)
+        encryptor = encrypt.Encryption(password=enc_key, salt=salt)
+        # Filepath is the path for the file
+        # Fileout is the name of the file, comes out with added
+        # _encryted prefix after ecnryption
+        encryptor.encrypt_with_aes(filepath, fileout)
+        print("Done encrypting")
 
     def tab_enc_files(self):
         """
@@ -167,21 +195,22 @@ class Encrypt_page:
         layout.addWidget(enc_text_label, 2, 0, 1, 1)
         layout.addWidget(enc_conf_label, 2, 2)
         # ENCRYPTION KEY INPUT AND CONFIRM 
-        text_box_enc_text = PasswordEdit()
-        text_box_enc_text_confirm = PasswordEdit()
-        layout.addWidget(text_box_enc_text, 2, 1)
-        layout.addWidget(text_box_enc_text_confirm, 2, 3)
+        self.text_box_enc_text = PasswordEdit()
+        self.text_box_enc_text_confirm = PasswordEdit()
+        layout.addWidget(self.text_box_enc_text, 2, 1)
+        layout.addWidget(self.text_box_enc_text_confirm, 2, 3)
 
         # SALT INPUT LABEL
         salt_label = QLabel(self.translations["labels"]["salt_label"])
         layout.addWidget(salt_label, 3, 0, 1, 1)
         # SALT INPUT
-        salt_insert_box = PasswordEdit()
-        layout.addWidget(salt_insert_box, 3, 1, 1, 3)
+        self.salt_insert_box = PasswordEdit()
+        layout.addWidget(self.salt_insert_box, 3, 1, 1, 3)
 
         # ENCRYPT BUTTON
-        encrypt_button = QPushButton(self.translations["buttons"]["final_encrypt"])
-        layout.addWidget(encrypt_button, 4, 0, 1, 4)
+        self.encrypt_button = QPushButton(self.translations["buttons"]["final_encrypt"])
+        layout.addWidget(self.encrypt_button, 4, 0, 1, 4)
+        self.encrypt_button.clicked.connect(self.encrypt_file)
         
         # finish and set layout
         main = QWidget()
