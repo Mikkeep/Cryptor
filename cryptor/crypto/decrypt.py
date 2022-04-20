3f92dd814b8e05c7df6f2670684c9115eb84dcbd
Mikkeep <mikke.pennanen@gmail.com>
2022-04-20 15:38:15 +0300

    Added fileout parameter for writing out decrypted files properly

--- cryptor/crypto/decrypt.py
+++ cryptor/crypto/decrypt.py
@@ -29,7 +29,7 @@ class Decryption:
         read_file = open(filename, "rb")
         return read_file
 
-    def decrypt_with_aes(self, filename):
+    def decrypt_with_aes(self, filename, fileout):
 
         try:
             file_in = open(filename, "rb")
@@ -39,7 +39,7 @@ class Decryption:
             cipher = AES.new(self.dec_key, AES.MODE_EAX, nonce)
             data = cipher.decrypt_and_verify(ciphertext, tag)
 
-            self.write_file(filename + ".dec", data.decode())
+            self.write_file(fileout + ".dec", data.decode())
 
             return 0
 
@@ -47,12 +47,15 @@ class Decryption:
             print("Failed decryption!")
             return -1
 
-    def decrypt_with_rsa(self, filename, priv_key):
+    def decrypt_with_rsa(self, filename, priv_key, fileout):
 
         try:
             file_in = self.open_file(filename)
 
-            private_key = RSA.import_key(open(priv_key).read())
+            try:
+                private_key = RSA.import_key(open(priv_key).read())
+            except OSError as e_msg:
+                return -2
 
             enc_session_key, nonce, tag, ciphertext = [
                 file_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1)
@@ -66,7 +69,7 @@ class Decryption:
             cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
             data = cipher_aes.decrypt_and_verify(ciphertext, tag)
 
-            self.write_file(filename + ".dec", data.decode())
+            self.write_file(fileout + ".dec", data.decode())
 
             return 0
 
@@ -74,7 +77,7 @@ class Decryption:
             print("Failed decryption!")
             return -1
 
-    def decrypt_with_chacha(self, filename):
+    def decrypt_with_chacha(self, filename, fileout):
 
         try:
             file_in = open(filename, "rb")
@@ -85,7 +88,7 @@ class Decryption:
             cipher = ChaCha20_Poly1305.new(key=self.dec_key, nonce=nonce)
             data = cipher.decrypt_and_verify(ciphertext, tag)
 
-            self.write_file(filename + ".dec", data.decode())
+            self.write_file(fileout + ".dec", data.decode())
 
             return 0
 
